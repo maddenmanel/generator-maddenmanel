4175f9937c2aff72c21f8996aa48a0cdcbb67af3
maddenrichard <zhangxuegang@gmail.com>
2025-02-22 00:55:07 +0800

    Modify the node version.

--- api/index.js
+++ api/index.js
@@ -1,94 +1,97 @@
 'use strict';
-var util = require('util');
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var mkdirp = require('mkdirp');
 
-var SpringGenerator = module.exports = function SpringGenerator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
-};
+const Generator = require('yeoman-generator');  // Correct import
+const chalk = require('chalk');
+const mkdirp = require('mkdirp'); // Used to create directories
 
-util.inherits(SpringGenerator, yeoman.generators.Base);
+// Define the Spring generator class
+module.exports = class SpringGenerator extends Generator {
+  constructor(args, options) {
+    super(args, options);  // Call the parent constructor
+  }
 
-SpringGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
+  // Asynchronous method for prompting the user
+  async askFor() {
+    // console.log(chalk.green(`
+    //   .............DD88888888888888888,............
+    //   ...........:888888888888888888888,...........
+    //   ..........+88888888888888888888888+..........
+    //   .........,8888888888888888888888888..........
+    //   .........888888888888...888888888888.........
+    //   .......,88888887..D88...88Z..88888888,.......
+    //   .......8888888,...888...88D...=8888888.......
+    //   ......D888888,..$8888...88887...8888888......
+    //   .....Z888888$..I88888...88888:..88888888,....
+    //   ....D8888888...888888...88888D..,88888888....
+    //   ....88888888,..888888..,888888...88888888....
+    //   ....88888888,..8888888$888888D..,88888888....
+    //   ....88888888I..Z8888888888888+..888888888....
+    //   .....Z8888888...O888888888888..,88888888.....
+    //   ......88888888...,88888888D...,88888888......
+    //   .......88888888=.....?I+.....I88888888.......
+    //   .......,88888888D7.........ZD88888888,.......
+    //   .........888888888888888888888888888.........
+    //   .........,8888888888888888888888888..........
+    //   ..........+88888888888888888888888+..........
+    //   ...........,888888888888888888888:...........
+    //   .............DD888888888888888DD.............
+    // `));
 
-  console.log(chalk.green('\n.............DD88888888888888888,............\n' +
-    '...........:888888888888888888888,...........\n' +
-    '..........+88888888888888888888888+..........\n' +
-    '.........,8888888888888888888888888..........\n' +
-    '.........888888888888...888888888888.........\n' +
-    '.......,88888887..D88...88Z..88888888,.......\n' +
-    '.......8888888,...888...88D...=8888888.......\n' +
-    '......D888888,..$8888...88887...8888888......\n' +
-    '.....Z888888$..I88888...88888:..88888888,....\n' +
-    '....D8888888...888888...88888D..,88888888....\n' +
-    '....88888888,..888888..,888888...88888888....\n' +
-    '....88888888,..8888888$888888D..,88888888....\n' +
-    '....88888888I..Z8888888888888+..888888888....\n' +
-    '.....Z8888888...O888888888888..,88888888.....\n' +
-    '......88888888...,88888888D...,88888888......\n' +
-    '.......88888888=.....?I+.....I88888888.......\n' +
-    '.......,88888888D7.........ZD88888888,.......\n' +
-    '.........888888888888888888888888888.........\n' +
-    '.........,8888888888888888888888888..........\n' +
-    '..........+88888888888888888888888+..........\n' +
-    '...........,888888888888888888888:...........\n' +
-    '.............DD888888888888888DD.............\n' +
-    chalk.yellow('\nWelcome to the Spring Boot Generator\n\nLets get started!\n\n')));
+    // console.log(chalk.yellow('\nWelcome to the Spring Boot Generator\n\nLets get started!\n\n'));
 
+    // Define prompts
+    const prompts = [
+      {
+        type: 'input',
+        name: 'packageName',
+        message: '请输入包名:',
+        default: 'com.maddenmanel'
+      },
+      {
+        type: 'input',
+        name: 'systemName',
+        message: '请输入应用名称:',
+        default: 'app'
+      }
+    ];
 
-  var prompts = [
-    {
-      type: 'string',
-      name: 'packageName',
-      message: '请输入包名:',
-      default: 'com.maddenmanel'
-    }, {
-      type: 'string',
-      name: 'systemName',
-      message: '请输入应用名称:',
-      default: 'app'
-    }
-  ];
+    // Prompt the user and capture their answers
+    const props = await this.prompt(prompts);
 
-  this.prompt(prompts, function (props) {
-    console.log("包名：" + this.packageName);
-    console.log("系统名：" + this.systemName);
-    console.log("基础名：" + this.baseName);
-
+    // Store user input
     this.packageName = props.packageName;
     this.systemName = props.systemName;
-    this.baseName = this.systemName.replace(/\-/g,'.').replace(/\_/g,'.');
-    cb();
-  }.bind(this));
-};
+    this.baseName = this.systemName.replace(/\-/g, '.').replace(/\_/g, '.');  // Convert system name to base name
+  }
 
-SpringGenerator.prototype.app = function app() {
-  console.log(this.packageName);
-  var packageFolder = this.packageName.replace(/\./g, '/') + '/' + this.baseName.replace(/\./g,'/');
+  // This method is used to generate files based on user input
+  app() {
+    // Define directory paths based on the package name and system name
+    const packageFolder = this.packageName.replace(/\./g, '/') + '/' + this.baseName.replace(/\./g, '/');
+    const srcDir = `${this.systemName}/src/main/java/${packageFolder}`;
+    const moDir = `${this.systemName}/src/main/java/${packageFolder}/dto`;
+    const facadeDir = `${this.systemName}/src/main/java/${packageFolder}/facade`;
 
-  var srcDir = this.systemName + '/src/main/java/' + packageFolder;
-  var moDir = this.systemName + '/src/main/java/' + packageFolder + '/dto';
-  var facadeDir = this.systemName + '/src/main/java/' + packageFolder + '/facade';
+    // Create the necessary directories
+    mkdirp.sync(srcDir);
+    mkdirp.sync(moDir);
+    mkdirp.sync(facadeDir);
 
-  // Mkdir.
-  mkdirp(srcDir);
-  mkdirp(moDir);
-  mkdirp(facadeDir);
+    // Copy template files to the specified locations
+    this.copy('_gitignore', `${this.systemName}/.gitignore`);
+    this.template('pom.xml', `${this.systemName}/pom.xml`);
+    this.template('Request.java', `${srcDir}/Request.java`);
+    this.template('Response.java', `${srcDir}/Response.java`);
+    this.template('UserDTO.java', `${moDir}/UserDTO.java`);
+    this.template('UserServiceProvider.java', `${facadeDir}/UserServiceProvider.java`);
 
-  // Template.
-  this.copy('_gitignore', this.systemName + '/.gitignore');
-  this.template('pom.xml', this.systemName + '/pom.xml');
-  this.template('Request.java', srcDir + '/Request.java');
-  this.template('Response.java', srcDir + '/Response.java');
-  this.template('UserDTO.java', moDir + '/UserDTO.java');
-  this.template('UserServiceProvider.java', facadeDir + '/UserServiceProvider.java');
+    // Store config values
+    this.config.set('packageName', this.packageName);
+    this.config.set('packageFolder', packageFolder);
+  }
 
-  this.config.set('packageName', this.packageName);
-  this.config.set('packageFolder', packageFolder);
-};
-
-
-SpringGenerator.prototype.projectfiles = function projectfiles() {
+  // This method is a placeholder for future tasks
+  projectfiles() {
+    // No actions needed for now
+  }
 };
